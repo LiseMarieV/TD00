7353e1233e66399eadfe07ddbdb231b42c041f0e
LiseMarieV <89929217+LiseMarieV@users.noreply.github.com>
2022-09-02 11:53:53 +0200

    Initial commit

---
+++ TD_PROG_000/hello.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+// TD_PROG_000
+// 
+// cette solution crée un premier programme
+// que vous pouvez cloner localement à l'aide des fonctions GitHub intégrées dans Visual Studio
+// et exécuter afin de vérifier que votre environnement de développement se comporte correctement.
+// Pour cloner le dépôt, suivez les intructions données par votre enseignant pendant la séance.
+
+int main(int argc, char* argv[]) {
+
+
+
+	printf("\nHello JUNIA ISEN !\n");
+
+
+
+
+
+	return(EXIT_SUCCESS);
+}
