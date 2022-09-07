3002c863c00a8cc948279d2a13c005104f76aabc
Lise-Marie VEILLON <lise-marie.veillon@junia.com>
2022-09-07 11:43:45 +0200

    Un ensemble d'opérateurs logiques et arithmétiques

--- TD_PROG_000/hello.c
+++ TD_PROG_000/hello.c
@@ -14,9 +14,25 @@ int main(int argc, char* argv[]) {
 
 
 	printf("\nHello JUNIA ISEN !\n");
-	printf("test\n");
-	printf("test\n");
-
-
+	printf("7 + 2 = %d \n",7 + 2);
+	printf("7 * 2 = %d \n", 7 * 2);
+	printf("7 - 2 = %d \n", 7 - 2);
+	printf("7 / 2 = %d \n", 7 / 2); // division de nombres entier -> division entière (quotient)
+	printf("7 %% 2 = %d \n", 7 % 2); // modulo -> reste de la division entière
+	printf("7.0 / 2 = %f \n", 7.0 / 2); // division de nombres à virgules -> division à virgule
+	printf("7 << 2 = %d \n", 7 << 2); // décalage à gauche d'un nombre binaire : multiplier par 2 un certain nombre de fois'
+	printf("7 >> 2 = %d \n", 7 >> 2); // décalage à droite d'un nombre binaire : diviser par 2 un certain nombre de fois'
+	printf("7 << 3 = %d \n\n", 7 << 3);
+	printf("7 & 2 = %d \n", 7 & 2); // opération ET bit à bit (entre deux séquences de valeurs logiques)
+	printf("7 | 2 = %d \n", 7 | 2); // opération OU bit à bit
+	printf("7 && 2 = %d \n", 7 && 2); //opération ET logique (entre deux valeurs logiques)
+	printf("7 || 2 = %d \n", 7 || 2); // opération OU logique
+	
+	printf("0 & 2 = %d \n", 0 & 2);
+	printf("8 & 2 = %d \n", 8 & 2);
+	printf("0 | 2 = %d \n", 0 | 2);
+	printf("0 && 2 = %d \n", 0 && 2);
+	printf("0 || 2 = %d \n", 0 || 2);
+	
 	return(EXIT_SUCCESS);
 }
